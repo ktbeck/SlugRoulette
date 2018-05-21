0f03edde2f86152378bab96d1cafd636ce83bdc5
AmericanCommunist <Zeyuanjiang@gmail.com>
2018-05-20 22:11:47 -0700

    Add files via upload
    implementation so far for firebase

---
+++ default_index.js
@@ -0,0 +1,17 @@
+(function() {
+
+	const config ={
+
+		apiKey: "AIzaSyBzcBFPL6xIacLGi9h0l5NpiNnP04Jzrbs",
+		authDomain: "slug-roulette.firebaseapp.com",
+		databaseURL: "https://slug-roulette.firebaseio.com",
+		storageBucket: "slug-roulette.appspot.com",
+	
+	};
+	firebase.initalizeApp(config);
+	
+
+
+
+}());
+
