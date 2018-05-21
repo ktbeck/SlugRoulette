7657f690938f16aa45e1db33a7811c85b4ac7fb0
AmericanCommunist <Zeyuanjiang@gmail.com>
2018-05-21 00:19:26 -0700

    Add files via upload
    attempt 2

--- default_index.js
+++ default_index.js
@@ -10,8 +10,20 @@
 	};
 	firebase.initalizeApp(config);
 	
+	const txtEmail    = document.getELementById('txtEmail');
+	const txtPassword = document.getElementById('txtPassword');
+	const btnLogin    = document.getElementById('btnLogin');
+	const btnSignUp   = document.getElementById('btnSignUp');
+	const btnLogout   = document.getElementById('btnLogout');
 
+	btnLogin.addEventListener('click', e=> {
 
-
+		const email = txtEmail.value;
+		const pass  = txtPassword.value;
+		const auth  = firebase.auth();
+		alert('a');
+		const promise = auth.signInWithEmailAndPassword(email, password);
+		promise.catch(e => console.log(e.message));
+	});
 }());
 
